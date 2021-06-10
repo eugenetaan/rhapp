76df8888f8f95aa0f97276f252761cde8d67c663
bakwxh <omeag1653@gmail.com>
2021-06-10 23:21:18 +0800

    Add endpoint for updating multiple hasRecevied by orderId

--- backend/facilityBooking/facilities.py
+++ backend/facilityBooking/facilities.py
@@ -1035,6 +1035,34 @@ def collated_orders(supperGroupId):
         return make_response({"status": "failed", "err": str(e)}, 400)
 
 
+@app.route('/supper/supperGroup/<int:supperGroupId>/payment', methods=['PUT'])
+@cross_origin(supports_credentials=True)
+def order_payments(supperGroupId):
+    try:
+        if request.method == 'PUT':
+            data = request.get_json()
+
+            received_orders = []
+            not_received_orders = []
+            for order in data:
+                if order['hasReceived']:
+                    received_orders.append(ObjectId(order['orderId']))
+                else:
+                    not_received_orders.append(ObjectId(order['orderId']))
+
+            db.Order.update_many({"_id": {"$in": received_orders}},
+                                {"$set": {"hasReceived": True}})
+
+            db.Order.update_many({"_id": {"$in": not_received_orders}},
+                                {"$set": {"hasReceived": False}})
+
+            response = {"status": "success", "data": data}
+            return make_response(response, 200)
+    except Exception as e:
+        print(e)
+        return make_response({"status": "failed", "err": str(e)}, 400)
+
+
 @app.route('/supper/supperGroup/<int:supperGroupId>/user/<userID>', methods=['GET'])
 @cross_origin(supports_credentials=True)
 def user_order(supperGroupId, userID):
